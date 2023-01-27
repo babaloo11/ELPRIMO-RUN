4cd8c9360257fd47c9825b8181a4ce51276befc1
dashakareyka <87483180+dashakareyka@users.noreply.github.com>
2023-01-27 16:08:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,72 @@
-const width = 480;
-const height = 360;
-let draw = SVG("IGRA").size(width, height);
-let background = draw.image("ARENA.jpg", width, height);
+const WIDTH = 480;
+const HEIGHT = 360;
+
+let draw = SVG("game").size(WIDTH, HEIGHT);
+let background = draw.image("ARENA.jpg", WIDTH, HEIGHT);
 let ELPRIMO = draw.image("ELPRIMO.png", 80, 80).move(0, 230);
-let WALL = draw.image("WALL.png", 60,60).move(400, 230);
-let TEXT = draw.text("SCORE ").move(240, 30).font({size:40}).fill("WHITE");
+let WALL = draw.image("WALL.png", 60, 60).move(400, 230);
+let TEXT = draw.text("SCORE ").move(240, 30).font({ size: 40 }).fill("WHITE");
+
 let isJump = false;
 let changeY = 0;
-let POINT = draw.image("POINT.webp", 40,40).move(400,100);
+let POINT = draw.image("POINT.webp", 40, 40).move(400, 100);
 let SCORE = 0;
- 
-function update()
-{
-if(isJump){
-    ELPRIMO.dy(changeY);
-    changeY += 0.5;
-    if(ELPRIMO.y() >= 230 ){
-        isJump = false;
+
+function update() {
+    if (isJump) {
+        ELPRIMO.dy(changeY);
+        changeY += 0.5;
+        if (ELPRIMO.y() >= 230) {
+            isJump = false;
+        }
     }
+
+    WALL.dx(-4);
+    if (WALL.x() < -50) {
+        WALL.x(WIDTH)
+    }
+    document.addEventListener("mousedown", function (event) {
+        if (isJump == false) {
+            changeY = -14;
+            isJump = true;
+        }
+    });
+
+
+    let collision = ELPRIMO.x() + ELPRIMO.width() > WALL.x() &&
+        WALL.x() + WALL.width() > ELPRIMO.x() &&
+        ELPRIMO.y() + ELPRIMO.height() > WALL.y();
+
+    if (collision) {
+        background.load("gmo.png")
+        clearInterval(update_id);
+    }
+    POINT.dx(-3);
+    if (POINT.x() < -50) {
+        POINT.x(WIDTH)
+    }
+    let collision2 = ELPRIMO.x() + ELPRIMO.width() > POINT.x() &&
+        POINT.x() + POINT.width() > ELPRIMO.x() &&
+        ELPRIMO.y() < POINT.y();
+    if (collision2) {
+        SCORE += 1
+        TEXT.text("" + SCORE);
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-WALL.dx(-4);
-if(WALL.x() < -50){
-    WALL.x(width)
-}
-document.addEventListener("mousedown", function(event)
-{
-if(isJump == false){
-changeY = -14;
-isJump = true;
-}
-});
- 
-
-let collision = ELPRIMO.x() + ELPRIMO.width() > WALL.x() && 
-                WALL.x() + WALL.width() > ELPRIMO.x() &&
-                ELPRIMO.y() + ELPRIMO.height() > WALL.y();  
-            
-if(collision){
-   background.load("gmo.png")
-   clearInterval(update);
-}
-POINT.dx(-3);
-if(POINT.x() < -50){
-   POINT.x(width)
-}          
-let collision2 = ELPRIMO.x() + ELPRIMO.width() > POINT.x() && 
-POINT.x() + POINT.width() > ELPRIMO.x() &&
-ELPRIMO.y() < POINT.y(); 
-if(collision2){
-    SCORE += 1
-    TEXT.text(""+SCORE);
-};
+let update_id = setInterval(update, 10);
 
 
-
-
-
-
-
-
-
-
-
-
-
-}
-setInterval(update,10);
 
